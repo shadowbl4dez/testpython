00f30daa659860ec140e1f99ee6b568beeecf392
shadowbl4dez <lubusa@web.de>
2020-10-24 20:20:47 +0200

    FirstTry

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("hello world")
+greeting = "Hello"
+
+name = "world"
+
+name = name.replace("world", "Augsburg")
+
+message = f"{greeting} {name.upper()}!"
+
+print(message)
+
+num_1 = "123"
+num_2 = "321"
+
+num_1 = float("123")
+num_2 = int("321")
+
+print(num_1 + num_2)
